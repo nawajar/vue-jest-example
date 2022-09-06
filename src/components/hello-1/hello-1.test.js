77734c17a811acdde4560b800642270a973a12a4
navaj565 <nawa@odds.team>
2022-09-06 12:38:09 +0700

    test with jest

--- src/components/hello-1/hello-1.test.js
+++ src/components/hello-1/hello-1.test.js
@@ -4,7 +4,13 @@ import Hello1 from "./hello-1.vue";
 
 describe('Hello1', () => {
     it('should display text hello1', () => {
-        const wrapper = mount(ParentComponent);
+        const wrapper = mount(Hello1);
         expect(wrapper.text()).toContain('hello 1');
+    });
+
+    it('should button click with doSomething', async() => {
+        const mockMethod = jest.spyOn(Hello1.methods, 'doSomething');
+        await mount(Hello1).find('#click-btn').trigger('click');
+        expect(mockMethod).toHaveBeenCalled();
     });
 });
--- src/components/hello-1/hello-1.vue
+++ src/components/hello-1/hello-1.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="w-full">hello 1</div>
+  <div class="w-full">hello 1
+    <button id="click-btn" @click="doSomething">click</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Hello1",
   computed: {},
+  methods: {
+    doSomething: function(event) {
+      console.log('click!');
+    }
+  }
 };
 </script>
