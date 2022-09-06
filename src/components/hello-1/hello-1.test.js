d5fbc7ce55f8f68c59d50d7f1f98d9d59ff4835f
navaj565 <nawa@odds.team>
2022-09-06 13:15:28 +0700

    test jest

--- src/components/hello-1/hello-1.test.js
+++ src/components/hello-1/hello-1.test.js
@@ -1,25 +1,32 @@
-import { mount } from '@vue/test-utils';
+import { mount } from "@vue/test-utils";
 import Hello1 from "./hello-1.vue";
 
+describe("Hello1", () => {
+  let wrapper = mount(Hello1);
 
-describe('Hello1', () => {
-    //dom expect view
-    it('should counter when doSomething', async () => {
-        const wrapper = mount(Hello1);
-        wrapper.setData({ count: 0 });
+  beforeEach(() => {
+    wrapper.setData({ count: 0 });
+  });
 
-        await wrapper.find('#click-btn').trigger('click');
+  //mock with jest
+    //   it("click does something", async () => {
+    //     const mockMethod = jest.spyOn(wrapper.vm, "doSomething");
+    //     await wrapper.find("#click-btn").trigger("click");
+    //     expect(mockMethod).toHaveBeenCalled();
+    //   });
 
-        expect(wrapper.text()).toContain('hello 1');
-    });
+  //dom expect view
+  it("should counter when doSomething", async () => {
+    await wrapper.find("#click-btn").trigger("click");
 
-    //properties expect
-    it('should counter when doSomething', async () => {
-        const wrapper = mount(Hello1);
-        wrapper.setData({ count: 0 });
+    expect(wrapper.text()).toContain("hello 1");
+  });
 
-        wrapper.vm.doSomething();
+  //component properties expect
+  it("should counter when doSomething", () => {
+    wrapper.vm.doSomething();
 
-        expect(wrapper.vm.count).toBe(1);
-    });
+    expect(wrapper.vm.count).toBe(1);
+  });
+  
 });
